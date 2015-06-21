cba66e50d4384f82e02c0b32a308602fb5a0bb1d
Eckingsan <brendan.eck@gmail.com>
2015-06-21 14:57:45 -0400

    Cache Matrix for Programming Assignment 2
    Made the changes as described in the readme file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2
+## R Programming 2015
+## Brendan Eck
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This generates the cached version (environment) of the matrix "X".
 
+makeCacheMatrix <- function(X = numeric()) {
+  
+  # Below we set the four fields which make up the "CacheMatrix" data structure
+  # Null out the inverse (we do not yet have it)
+  m <- NULL
+  
+  # Set the value of the matrix. Only use this if you want to
+  # change the matrix "X" to matrix "Y". Not sure why this is useful....
+  # May be useful in some other "CacheFunctions" like CacheSolve.R which use
+  # this environment.
+  set <- function(Y) {
+    X <<- Y
+    m <<- NULL
+  }
+  
+  # Get the original matrix, return it to workspace
+  get <- function() X
+  
+  # Assign the inverted matrix to the data structure
+  setinv <- function(inv) m <<- inv
+  
+  # Get the inverted matrix, return it to workspace
+  getinv <- function() m
+  
+  # Output the allocated "CacheMatrix"
+  output <- list(set = set, get = get,
+                 setinv = setinv,
+                 getinv = getinv)
+  
+  output 
 }
 
+## Computes or accesses cached inverse of the cache matrix "X".
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(X, ...) {
+  # First, check to see if the input "CacheMatrix" already has had its inverse
+  # computed
+  m <- X$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  # If getinv(X) returned "NULL", then we need to compute the matrix inversion
+  message("No cached data found. Performing matrix inversion..")
+  data <- X$get()
+  m <- solve(data, ...)
+  X$setinv(m)
+  m
 }
